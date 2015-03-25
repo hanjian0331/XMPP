58d700b04ae2a0af8eb7fddeddd9809e8c8acf72
HJ <hj@HJdeMacBook-Air.local>
2015-03-25 16:46:20 +0800

    1

---
+++ XMPP(框架导入)/XMPP/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  XMPP
+//
+//  Created by HJ on 15/3/25.
+//  Copyright (c) 2015年 HJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+- (void)logout;
+
+@end
+
